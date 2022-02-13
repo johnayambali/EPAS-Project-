33a50ffbf8b05dd5971bfa9dda5a60305e35ddd7
PaulFortin2 <pfort092@uottawa.ca>
2022-02-13 13:00:34 -0500

    Database integration
    Integrated database for user creation and project postings

--- epas/projectListing/views.py
+++ epas/projectListing/views.py
@@ -1,48 +1,10 @@
 from django.shortcuts import render
-
-postedProject = [
-    {
-        'professor': 'Daniel Amyot',
-        'title': 'epas',
-        'description': 'description of the project',
-        'date_posted': 'Febuary 3rd 2022'
-    },
-    {
-        'professor': 'Amy Felty',
-        'title': 'software develpment',
-        'description': 'description of the project',
-        'date_posted': 'Febuary 1st 2022'
-    },
-    {
-        'professor': 'Amy Felty',
-        'title': 'software development',
-        'description': 'description of the project',
-        'date_posted': 'Febuary 1st 2022'
-    },
-    {
-        'professor': 'Amy Felty',
-        'title': 'software development',
-        'description': 'description of the project',
-        'date_posted': 'Febuary 1st 2022'
-    },
-    {
-        'professor': 'Amy Felty',
-        'title': 'software development',
-        'description': 'description of the project',
-        'date_posted': 'Febuary 1st 2022'
-    },
-    {
-        'professor': 'Amy Felty',
-        'title': 'software development',
-        'description': 'description of the project',
-        'date_posted': 'Febuary 1st 2022'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'postedProject': postedProject
+        'postedProject': Post.objects.all()
     }
     return render(request, 'projectListing/home.html', context)
 
