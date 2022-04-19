ce081e0733a6025688044fc087c82c177b14aa1b
emiliedrouin1 <71474337+emiliedrouin1@users.noreply.github.com>
2022-04-19 14:45:01 -0400

    Removed unnecessary urls

--- epas/projectListing/urls.py
+++ epas/projectListing/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
 from . import views
-from .views import PostCreateView, PostListView, PostDetailView, PostUpdateView, PostDeleteView, ApplicationCreateView, ApplicationListView, ApplicationDetailView, ApplicationUpdateView, ApplicationDeleteView, UpdateApprovalView, UpdateStatusView, UpdateOfferView
+from users import views as user_views
+from .views import PostCreateView, PostListView, PostDetailView, PostUpdateView, PostUpdateView2, PostUpdateView3, PostDeleteView, ApplicationCreateView, ApplicationListView, ApplicationDetailView, ApplicationUpdateView, ApplicationDeleteView, UpdateApprovalView, UpdateStatusView, UpdateOfferView
 
 urlpatterns = [
     path('', views.home, name='projectListing-home'),
-    path('profile/', views.profile, name='projectListing-profile'),
+    path('profile/', user_views.profile, name='projectListing-profile'),
     path('approval/', views.homeDirector, name='projectListing-homeDirector'),
     path('myprojects/', views.myprojects, name='projectListing-myprojects'),
     path('applications/', views.applications, name='projectListing-applications'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/assign', PostUpdateView2.as_view(), name='post-assign'),
+    path('post/<int:pk>/complete', PostUpdateView3.as_view(), name='post-complete'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/apply', ApplicationCreateView.as_view(), name='application-create'),
     path('application/<int:pk>/', ApplicationDetailView.as_view(), name='application-detail'),
