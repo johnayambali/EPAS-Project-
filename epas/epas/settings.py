7d19885e77a8c039d9360c43d3174e6d36f8b4c4
PaulFortin2 <pfort092@uottawa.ca>
2022-04-21 23:41:29 -0400

    Internationalization first commit

--- epas/epas/settings.py
+++ epas/epas/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -117,6 +118,11 @@ USE_L10N = True
 USE_TZ = True
 LOCALE_PATHS = [
     os.path.join(BASE_DIR, 'locale')
+]
+
+LANGUAGES = [
+    ('en', 'English'),
+    ('fr', 'francais'),
 ]
 
 
